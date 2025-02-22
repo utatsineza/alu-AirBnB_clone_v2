ee83ba4bdecb792324999138cd0fc545a5f79c09
utatsineza <h.utatsinez@alustudent.com>
2025-02-22 09:18:32 -0800

    cloning

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 Merge remote-tracking branch 'origin/master'
 # Please enter a commit message to explain why this merge is necessary,
 # especially if it merges an updated upstream into a topic branch.
